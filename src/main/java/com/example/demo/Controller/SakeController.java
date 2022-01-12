9bb6f8d5d0cf1d6315a1a12685e658f244b225c6
yi-inaba <drive19anywhere@gmail.com>
2022-01-12 19:42:36 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/Controller/SakeController.java
@@ -0,0 +1,14 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SakeController {
+	
+	@GetMapping
+	public String index() {
+		return "index";
+	}
+//テスト
+}
---
+++ src/main/java/com/example/demo/RaiseTechJavaTheme0520220112Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaiseTechJavaTheme0520220112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaiseTechJavaTheme0520220112Application.class, args);
+	}
+
+}
